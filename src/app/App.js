07165ef86d00018f3cdae8fb6e5a05a8466cf328
Umidjon1717 <umidizzatullayev1717@gmail.com>
2025-01-20 12:10:22 +0500

    snowfall added

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
+import Snowfall from "react-snowfall"; 
 import "./App.css";
 
 function _ScrollToTop(props) {
@@ -16,12 +14,22 @@ function _ScrollToTop(props) {
   }, [pathname]);
   return props.children;
 }
+
 const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop>
+        <Snowfall
+          snowflakeCount={100}
+          style={{
+
+            width: "100vw",
+            height: "100vh",
+            zIndex: -1, 
+          }}
+        />
         <Headermain />
         <AppRoutes />
       </ScrollToTop>
